7c98e84c3bbe0305cbb8bc064a7fc79b1e38c6b0
Ivan Monichev <ivan@monichev.ru>
2022-08-27 13:14:38 +0300

    feat: add layout promo

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
+import Promo from '../Promo/Promo';
 
 function Main() {
   return (
     <main className="main">
-      <section className="promo">
-        <div className="promo__wrapper">
-          <div className="promo__content">
-            <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-            <p className="promo__description">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-            <a href="/" className="promo__button">Узнать больше</a>
-          </div>
-          <img className="promo__image" src="../../images/web-globe.svg" alt="Планета Земля из надписей «Web»" />
-        </div>
-      </section>
+      <Promo />
     </main>
   );
 }
