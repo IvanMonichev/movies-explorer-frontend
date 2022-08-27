4bfc7202cdb5738e9a95d8f723e13b4b59f160e9
Ivan Monichev <ivan@monichev.ru>
2022-08-27 18:43:54 +0300

    feat: add layout about project

---
+++ src/components/AboutProject/AboutProject.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function aboutProject() {
+  return (
+    <section className="about-project">
+      <div className="about-project__wrapper">
+        <h2 className="about-project__title main__title">О проекте</h2>
+        <div className="about-project__content">
+          <div className="about-project__text">
+            <h2 className="process-text__title">Дипломный проект включал 5 этапов</h2>
+            <p className="process-text__paragraph">
+              Составление плана, работу над бэкендом,
+              вёрстку, добавление функциональности и финальные доработки.
+            </p>
+          </div>
+          <div className="about-project__text">
+            <h2 className="process-text__title">На выполнение диплома ушло 5 недель</h2>
+            <p className="process-text__paragraph">
+              У каждого этапа был мягкий и жёсткий дедлайн,
+              которые нужно было соблюдать, чтобы успешно защититься.
+            </p>
+          </div>
+        </div>
+        <div className="about-project__timeline timeline">
+          <div className="timeline__backend timeline__item">
+            <span className="timeline__backend-time timeline__text">1 неделя</span>
+            <span className="timeline__name timeline__text">Back-end</span>
+          </div>
+          <div className="timeline__frontend timeline__item">
+            <span className="timeline__frontend-time timeline__text">4 недели</span>
+            <span className="timeline__name timeline__text">Front-end</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default aboutProject;
