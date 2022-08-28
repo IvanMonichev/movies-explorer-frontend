46b680847310582c1547953e8ec19132cb08da3f
Ivan Monichev <ivan@monichev.ru>
2022-08-28 19:00:36 +0300

    feat: add layout footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 function Header() {
+  const date = new Date();
   return (
     <footer className="footer">
-      <h2>Footer</h2>
+      <div className="footer__wrapper">
+        <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+        <div className="footer__body">
+          <p className="footer__year">
+            ©
+            {date.getFullYear()}
+          </p>
+          <nav className="menu footer-menu">
+            <ul className="menu__list footer-menu__list">
+              <li className="footer-menu__item">
+                <a href="https://practicum.yandex.ru/" className="footer-menu__link link">Яндекс.Практикум</a>
+              </li>
+              <li className="menu__item">
+                <a href="https://github.com/IvanMonichev" className="footer-menu__link link">Github</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </footer>
   );
 }
