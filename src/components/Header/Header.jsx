9c45f8b2aec5590389d50e7478a61be030acab40
Ivan Monichev <ivan@monichev.ru>
2022-08-29 21:49:16 +0300

    feat: add gray theme for footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 import Navigation from '../Navigation/Navigation';
 
 function Header() {
+  const pathName = useLocation().pathname;
+
   return (
-    <header className="header">
+    <header className={pathName === '/' ? 'header_gray' : 'header'}>
       <div className="header__wrapper">
         <Link to="/" className="header__link-logo"><img src={logo} alt="Логотип" /></Link>
         <Navigation />
