07b26fee9684470fd396997dbf38abb74bb2557e
Ivan Monichev <ivan@monichev.ru>
2022-09-03 00:09:21 +0300

    feat: add register layout

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -11,27 +11,32 @@ function Register() {
       />
       <main>
         <section className="register">
-          <Link to="/" className="logo"><img src={logo} alt="Логотип" /></Link>
-          <h1 className="register__title">Добро пожаловать!</h1>
-          <form className="form-register register__form">
-            <label htmlFor="name" className="form-register__label">
-              Имя
-              <input type="text" className="register__input-name" name="name" id="name" required="" />
-            </label>
-            <label htmlFor="email" className="form-register__label">
-              E-mail
-              <input type="password" className="form-register__input-email" name="email" id="email" required="" />
-            </label>
-            <label htmlFor="name" className="form-register__label">
-              Пароль
-              <input type="password" className="form-register__input-password" name="password" id="password" required="" />
-            </label>
-            <button type="submit" className="form-register__button">Зарегистрироваться</button>
-          </form>
-          <p className="register__text">
-            Уже зарегистрированы?
-            <Link to="/sign-in" className="register__link">Войти</Link>
-          </p>
+          <div className="register__wrapper">
+            <Link to="/" className="logo"><img src={logo} alt="Логотип" /></Link>
+            <h1 className="register__title">Добро пожаловать!</h1>
+            <form className="form-register register__form">
+              <label htmlFor="name" className="form-register__label">
+                Имя
+                <input type="text" className="form-register__input" name="name" id="name" required />
+                <span className="form-register__error">Что-то пошло не так...</span>
+              </label>
+              <label htmlFor="email" className="form-register__label">
+                E-mail
+                <input type="password" className="form-register__input" name="email" id="email" required />
+                <span className="form-register__error">Что-то пошло не так...</span>
+              </label>
+              <label htmlFor="name" className="form-register__label">
+                Пароль
+                <input type="password" className="form-register__input" name="password" id="password" required />
+                <span className="form-register__error">Что-то пошло не так...</span>
+              </label>
+              <button type="submit" className="form-register__button">Зарегистрироваться</button>
+              <p className="register__text">
+                Уже зарегистрированы?
+                <Link to="/sign-in" className="register__link">Войти</Link>
+              </p>
+            </form>
+          </div>
         </section>
       </main>
     </>
