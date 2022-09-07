79ac01ddc0c9c6bed96563e932e3e39c3b6763e6
Ivan Monichev <ivan@monichev.ru>
2022-09-07 23:44:44 +0300

    feat: add profile layaout

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,27 @@ function Profile() {
       <HeadMain
         titleName="Movies Express | Профиль"
       />
-      <p>Profile</p>
+      <main>
+        <section className="form form-profile">
+          <div className="form__wrapper form-profile__wrapper">
+            <h1 className="form__title form-profile__title">Привет, Виталий!</h1>
+            <form className="form-body register__form">
+              <label htmlFor="name" className="form-body__label form-profile__label">
+                Имя
+                <input type="text" className="form-body__input form-profile__input form-profile__input_separate" value="Виталий" name="name" id="name" required />
+                <span className="form-body__error">Что-то пошло не так...</span>
+              </label>
+              <label htmlFor="email" className="form-body__label form-profile__label">
+                E-mail
+                <input type="email" className="form-body__input form-profile__input" value="pochta@yandex.ru" name="email" id="email" required />
+                <span className="form-body__error">Что-то пошло не так...</span>
+              </label>
+              <button type="button" className="form-profile__text">Редактировать</button>
+              <button type="button" className="form-profile__text form-profile__text_important">Выйти из аккаунта</button>
+            </form>
+          </div>
+        </section>
+      </main>
     </section>
   );
 }
