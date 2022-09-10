c20925582805735f353448a64a7482116101c3cb
Ivan Monichev <ivan@monichev.ru>
2022-09-10 15:38:09 +0300

    feat: add profile layout

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,32 +3,42 @@ import HeadMain from '../HeadMain/HeadMain';
 
 function Profile() {
   return (
-    <section>
-      <HeadMain
-        titleName="Movies Express | Профиль"
-      />
-      <main>
-        <section className="form form-profile">
-          <div className="form__wrapper form-profile__wrapper">
-            <h1 className="form__title form-profile__title">Привет, Виталий!</h1>
-            <form className="form-body register__form">
-              <label htmlFor="name" className="form-body__label form-profile__label">
-                Имя
-                <input type="text" className="form-body__input form-profile__input form-profile__input_separate" value="Виталий" name="name" id="name" required />
-                <span className="form-body__error">Что-то пошло не так...</span>
-              </label>
-              <label htmlFor="email" className="form-body__label form-profile__label">
-                E-mail
-                <input type="email" className="form-body__input form-profile__input" value="pochta@yandex.ru" name="email" id="email" required />
-                <span className="form-body__error">Что-то пошло не так...</span>
-              </label>
-              <button type="button" className="form-profile__text">Редактировать</button>
-              <button type="button" className="form-profile__text form-profile__text_important">Выйти из аккаунта</button>
-            </form>
-          </div>
-        </section>
-      </main>
-    </section>
+    <>
+      <HeadMain titleName="Movies Express | Профиль" />
+
+      <section className="form form-profile">
+        <div className="form__wrapper form-profile__wrapper">
+          <h1 className="form__title form-profile__title">Привет, Виталий!</h1>
+          <form className="form-body register__form">
+            <label htmlFor="name" className="form-body__label form-profile__label">
+              Имя
+              <input
+                type="text"
+                className="form-body__input form-profile__input form-profile__input_separate"
+                name="name"
+                id="name"
+                required
+              />
+              <span className="form-body__error">Что-то пошло не так...</span>
+            </label>
+            <label htmlFor="email" className="form-body__label form-profile__label">
+              E-mail
+              <input
+                type="email"
+                className="form-body__input form-profile__input"
+                name="email"
+                id="email"
+                required
+              />
+              <span className="form-body__error">Что-то пошло не так...</span>
+            </label>
+            <button type="button" className="form-profile__text form-profile__text_separator">Редактировать</button>
+            <button type="button" className="form-profile__text form-profile__text_important">Выйти из аккаунта</button>
+            <button type="submit" className="form-body__button form-body__button_inactive">Сохранить</button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
 
