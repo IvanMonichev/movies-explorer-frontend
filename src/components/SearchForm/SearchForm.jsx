53428ac28b433f6a9989296bccc6141093d5e295
Ivan Monichev <ivan@monichev.ru>
2022-09-22 18:00:24 +0300

    feat: add validation

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function SearchForm({ onSearchSubmit, onHandleCheck, shortChecked }) {
   const [search, setSearch] = useState('');
+  const {
+    register,
+    formState: {
+      errors,
+    },
+    handleSubmit,
+  } = useForm();
 
   const handleChangeSearch = (event) => {
     setSearch(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSearchSubmit = (event) => {
     event.preventDefault();
     onSearchSubmit(search);
   };
@@ -15,15 +23,17 @@ function SearchForm({ onSearchSubmit, onHandleCheck, shortChecked }) {
   return (
     <section className="search">
       <div className="search__wrapper">
-        <form className="search-form__form" onSubmit={handleSubmit}>
+        <form className="search-form__form" onSubmit={handleSubmit(handleSearchSubmit)}>
           <input
             type="text"
-            name="search"
             className="search__input"
             placeholder="Фильм"
-            required
             onChange={handleChangeSearch}
+            {...register('search', {
+              required: 'Нужно ввести ключевое слово',
+            })}
           />
+          <span className="search__error">{errors.search && `${errors.search.message || 'Что-то пошло не так...'}`}</span>
           <button className="search__button" type="submit">Найти</button>
         </form>
         <label htmlFor="filter-checkbox" className="filter">
